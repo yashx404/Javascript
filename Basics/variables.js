ea56bd3a41610b58e1d93619f50c46782a8aeb49
Yash <yashvatsparth@gmail.com>
2025-05-15 07:40:40 +0000

    updated

---
+++ Basics/variables.js
@@ -0,0 +1,15 @@
+const accountID = 123
+let accountEmail = "yash@gmail.com"
+var accountPassword = "Pass@123"
+accountCity = "Delhi"
+let accountState
+
+//accountID = 321
+console.log(accountID);
+
+accountEmail = "yash1@gmail.com"
+
+accountPassword = "Pass@321"
+
+accountCity = "Mumbai"
+console.table([accountEmail, accountPassword, accountCity, accountState])
